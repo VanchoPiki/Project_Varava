6d6320a6a1d08aed6e91fac64105226b873c9e93
VanchoPiki <varava24102020@gmail.com>
2025-11-01 17:07:06 +0300

    Выполнил пз 4, вроде правильно, надеюсь

--- TEST/PZ_4/PZ_4_1.py
+++ TEST/PZ_4/PZ_4_1.py
@@ -1,21 +1,13 @@
 #Даны два целых числа А и В (А < Б). Найти произведение всех целых чисел от А до В включительно.
 #Ввод значений
-A= int(input())
-B = int(input())
-t = 1
-
-#Цикл
-while A<B:
-    A += 1
-    t *= A
-
-    print(t)
+try:
+    A, B = int(input("Введите первое число")), int(input("Введите первое число"))
+    t = 1
+    #Цикл
+    while A<B:
+        A += 1
+        t *= A
+        print(t)
 #Вывод ошибок
-try:
-    A, B = int(input()), int(input())
-except Exception as e:
-    print(f"Произошла ошибка: {e}")
-try:
-    B > A or A == A + 1 or t == t + A
-except Exception as e:
+except ValueError as e:
     print(f"Произошла ошибка: {e}")

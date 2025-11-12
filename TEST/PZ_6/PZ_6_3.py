809002f5da78bf3c5f1eaf8a9bc7e99e69c968a5
VanchoPiki <varava24102020@gmail.com>
2025-11-12 14:18:00 +0300

    Выполнил пз 6, вроде правильно, надеюсь правильно

--- TEST/PZ_6/PZ_6_3.py
+++ TEST/PZ_6/PZ_6_3.py
@@ -1,6 +1,8 @@
 """Дан список размера N. Возвести в квадрат все его локальные минимумы (то есть числа,
  меньшие своих соседей)."""
+#Импортирую нужную библиотеку
 import random
+#Ввод нужных значений
 try:
     a = int(input("Введите желаемую длину списка\n"))
     c = [random.randint(-100, 100) for _ in range(a)]
@@ -10,7 +12,7 @@ except ValueError or NameError as e:
 
 maxi = []
 b = []
-
+#Нахождение локальных минимумов
 try:
     for i in range(1, y):
         if c[i] < c[i - 1]:
@@ -18,10 +20,11 @@ try:
                 maxi.append(c[i])
 except NameError as e:
     print(e)
-
+#Возведение в квадрат локальных минимумов
 for i in maxi:
     i = i**2
     b.append(i)
+#Вывод списков
 try:
     print(f"\nСам список:     {c}\n")
     print(f"Локальные минимумы:     {maxi}\n")

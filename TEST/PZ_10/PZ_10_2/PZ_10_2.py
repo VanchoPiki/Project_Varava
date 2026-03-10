1f53bfc8bf8bc3615448b71b1d25d32b44b01a70
VanchoPiki <varava24102020@gmail.com>
2026-03-10 21:08:19 +0300

    Выполнил пз 10

---
+++ TEST/PZ_10/PZ_10_2/PZ_10_2.py
@@ -0,0 +1,28 @@
+"""2. Из предложенного текстового файла (text18-7.txt) вывести
+на экран его содержимое, количество букв в нижнем регистре.
+Сформировать новый файл, в который поместить текст в стихотворной
+форме предварительно поставив последнюю строку между второй и третьей."""
+
+print('\n')
+
+#вывод стихотворения
+with open("text18-7.txt", "r", encoding="utf-8") as f:
+    print(f.read())
+
+#количество букв в нижнем регистре
+with open("text18-7.txt", "r", encoding="utf-8") as f:
+    list = f.readlines()
+    t = 0
+    for i in list:
+        for _ in i:
+            if _.islower():
+                t += 1
+    print(f"\nколичество букв в нижнем регистре: {t}\n")
+
+#подстановка строки между второй и третьей
+with open("text18-7.txt", "r", encoding="utf-8") as f:
+    list = f.readlines()
+    list.insert(2, (list[6] + "\n"))
+    list.pop()
+    with open("text2.txt", "w", encoding="utf-8") as f:
+        f.writelines(list)

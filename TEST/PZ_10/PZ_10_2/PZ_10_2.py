42d9b5e143533ca4ef82a3a29bd652ad8545b8a6
VanchoPiki <varava24102020@gmail.com>
2026-03-10 21:19:59 +0300

    Выполнил пз 10

--- TEST/PZ_10/PZ_10_2/PZ_10_2.py
+++ TEST/PZ_10/PZ_10_2/PZ_10_2.py
@@ -4,25 +4,27 @@
 форме предварительно поставив последнюю строку между второй и третьей."""
 
 print('\n')
+try:
+    #вывод стихотворения
+    with open("text18-7.txt", "r", encoding="utf-8") as f:
+        print(f.read())
 
-#вывод стихотворения
-with open("text18-7.txt", "r", encoding="utf-8") as f:
-    print(f.read())
+    #количество букв в нижнем регистре
+    with open("text18-7.txt", "r", encoding="utf-8") as f:
+        list = f.readlines()
+        t = 0
+        for i in list:
+            for _ in i:
+                if _.islower():
+                    t += 1
+        print(f"\nколичество букв в нижнем регистре: {t}\n")
 
-#количество букв в нижнем регистре
-with open("text18-7.txt", "r", encoding="utf-8") as f:
-    list = f.readlines()
-    t = 0
-    for i in list:
-        for _ in i:
-            if _.islower():
-                t += 1
-    print(f"\nколичество букв в нижнем регистре: {t}\n")
-
-#подстановка строки между второй и третьей
-with open("text18-7.txt", "r", encoding="utf-8") as f:
-    list = f.readlines()
-    list.insert(2, (list[6] + "\n"))
-    list.pop()
-    with open("text2.txt", "w", encoding="utf-8") as f:
-        f.writelines(list)
+    #подстановка строки между второй и третьей
+    with open("text18-7.txt", "r", encoding="utf-8") as f:
+        list = f.readlines()
+        list.insert(2, (list[6] + "\n"))
+        list.pop()
+        with open("text2.txt", "w", encoding="utf-8") as f:
+            f.writelines(list)
+except TypeError as e:
+    print(f"Произошла ошибка - {e}")

2c37a1d599b7a06b414f8289ff94dfb273eb515d
VanchoPiki <varava24102020@gmail.com>
2026-05-23 16:47:29 +0300

    Выполнил пз 13

--- TEST/PZ_13/PZ_13.py
+++ TEST/PZ_13/PZ_13.py
@@ -8,13 +8,13 @@ def main():
         with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Шаблон ищет 4 цифры подряд
+        #шаблон ищет 4 цифры подряд
         year_pattern = r'\b\d{4}(?:\s*(?:гг\.|г\.|год[ау]?))\b|\b\d{4}\b'
 
-        # Находим все совпадения
+        #находим все совпадения
         years = re.findall(year_pattern, content)
 
-        # Вывод результатов
+        #вывод результатов
         print(f"Найдено элементов: {len(years)}")
         print("Список найденных годов:")
         print(years)

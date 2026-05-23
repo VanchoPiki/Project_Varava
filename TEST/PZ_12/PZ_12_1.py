22361279de4347fe1979052d286de461d14ac3fe
VanchoPiki <varava24102020@gmail.com>
2026-05-23 17:44:57 +0300

    Выполнил пз 12

--- TEST/PZ_12/PZ_12_1.py
+++ TEST/PZ_12/PZ_12_1.py
@@ -1,24 +1,26 @@
 """Вариант 7.
 1. В матрице элементы строки N (N задать с клавиатуры) увеличить на 3"""
+a = int(input("Введите размеры квадратной матрицы: "))
 
 #создание самой матрицы 3 на 3
-matrix = [[int(x) for x in input(f"Введите строку {i} через пробел: ").split()] for i in range(3)]
+
+matrix = [[int(x) for x in input(f"Введите строку {i} через пробел: ").split()] for i in range(a)]
 
 for row in matrix:
     print(row)
 
 try:
     n = int(input("\nВведите номер строки N для изменения: "))
-
     # Перебираем строки с их индексами
-    result_matrix = [
-        [val + 3 for val in row] if i == n-1 else row
-        for i, row in enumerate(matrix)
-    ]
+    if 1 <= n <= a:
+        result_matrix = [
+            [val + 3 for val in row] if i == n - 1 else row
+            for i, row in enumerate(matrix)
+        ]
 
-    print(f"\nМатрица после увеличения строки {n} на 3:")
-    for row in result_matrix:
-        print(row)
+        print(f"\nМатрица после увеличения строки {n} на 3:")
+        for row in result_matrix:
+            print(row)
 
 except ValueError as e:
     print(e)
--- TEST/PZ_12/PZ_12_2.py
+++ TEST/PZ_12/PZ_12_2.py
@@ -1,7 +1,7 @@
 """2. В матрице элементы последнего столбца заменить на -1."""
-
+a = int(input("Введите размеры квадратной матрицы: "))
 #создание самой матрицы 3 на 3
-matrix = [[int(x) for x in input(f"Введите строку {i} через пробел: ").split()] for i in range(3)]
+matrix = [[int(x) for x in input(f"Введите строку {i} через пробел: ").split()] for i in range(a)]
 
 print(f"\nМатрица начальная")
 for row in matrix:

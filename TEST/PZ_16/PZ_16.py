98b6334d3b9d4a3a66781306c846f59ebe066b8d
VanchoPiki <varava24102020@gmail.com>
2026-06-06 18:03:33 +0300

    Выполнил пз 16

--- TEST/PZ_16/PZ_16.py
+++ TEST/PZ_16/PZ_16.py
@@ -2,47 +2,44 @@
 Добавьте методы для сложения, вычитания и умножения матриц."""
 
 class Matrix:
-    def __init__(self, data):
+    def __init__(self, size, data):
+        self.size = size
         self.data = data
-        self.rows = len(data)
-        self.cols = len(data[0])
 
-    def plus(self, other):
-        result = [[self.data[i][j] + other.data[i][j] for j in range(self.cols)] for i in range(self.rows)]
-        return Matrix(result)
+    def add(self, other):
+        result = [[self.data[i][j] + other.data[i][j] for j in range(self.size)] for i in range(self.size)]
+        return Matrix(self.size, result)
 
-    def minus(self, other):
-        result = [[self.data[i][j] - other.data[i][j] for j in range(self.cols)] for i in range(self.rows)]
-        return Matrix(result)
+    def subtract(self, other):
+        result = [[self.data[i][j] - other.data[i][j] for j in range(self.size)] for i in range(self.size)]
+        return Matrix(self.size, result)
 
-    def multi(self, other):
-        result = [[0] * other.cols for _ in range(self.rows)]
-        for i in range(self.rows):
-            for j in range(other.cols):
-                for k in range(self.cols):
+    def multiply(self, other):
+        result = [[0] * self.size for _ in range(self.size)]
+        for i in range(self.size):
+            for j in range(self.size):
+                for k in range(self.size):
                     result[i][j] += self.data[i][k] * other.data[k][j]
-        return Matrix(result)
+        return Matrix(self.size, result)
 
 
-m1 = Matrix([[2, 2],
-             [3, 6]])
+m1 = Matrix(2, [[1, 2],
+                [3, 4]])
 
-m2 = Matrix([[4, 6],
-             [7, 9]])
+m2 = Matrix(2, [[5, 6],
+                [7, 8]])
 
 print("Исходная матрица 1:")
 for row in m1.data:
     print(row)
-print(f"Размер: {m1.rows}x{m1.cols}")
 
 print("\nИсходная матрица 2:")
 for row in m2.data:
     print(row)
-print(f"Размер: {m2.rows}x{m2.cols}")
 
-matrix_sum = m1.plus(m2)
-matrix_sub = m1.minus(m2)
-matrix_mul = m1.multi(m2)
+matrix_sum = m1.add(m2)
+matrix_sub = m1.subtract(m2)
+matrix_mul = m1.multiply(m2)
 
 print("\nРезультат сложения:")
 for row in matrix_sum.data:
